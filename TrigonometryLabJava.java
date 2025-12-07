0371189732a5ae63e07da4acc5d59e45c52c8b3b
karlnicolipelletero-ux <karlnicolipelletero@g.cjc.edu.ph>
2025-12-07 18:56:56 +0800

    Add files via upload

---
+++ TrigonometryLabJava.java
@@ -0,0 +1,10 @@
+
+package trigonometrylab.java;
+import java.util.Scanner;
+import static java.lang.Math.*;
+public class TrigonometryLabJava {
+    public static void main(String[] args) {
+        
+    }
+    
+}
